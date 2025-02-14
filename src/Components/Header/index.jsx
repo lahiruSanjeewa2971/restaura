e3d8e0d1e30f05617462d99d44e20986b25f58d9
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-02-14 22:53:16 +0530

    navBar finish

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import logo from "../../assets/logo.png";
+import { LINKS } from "../../constants";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -7,7 +10,61 @@ const NavBar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  return <div>NavBar</div>;
+  const handleScroll = (event, target) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(target);
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop - 80;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth",
+      });
+    }
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
+      <div className="flex w-full items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem] lg:rounded-full lg:shadow-lg">
+        <img src={logo} alt="logo" width={80} height={22} />
+        <div className="hidden space-x-6 lg:flex">
+          {LINKS.map((link, index) => (
+            <a
+              href={`#${link.targetId}`}
+              key={index}
+              className={`text-sm ${
+                index !== 0 ? "border-l-2 border-neutral-300/20 pl-2 " : ""
+              } hover:opacity-50`}
+              onClick={(e) => handleScroll(e, link.targetId)}
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+
+        <div className="lg:hidden">
+          <button onClick={toggleMobileMenu}>
+            {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="w-full backdrop-blur-lg lg:hidden">
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={`#${link.targetId}`}
+              className="block p-4 uppercase tracking-tighter"
+              onClick={(e) => handleScroll(e, link.targetId)}
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default NavBar;
